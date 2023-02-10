63dc5829ca7fde6f1402b5b5357212f457105f24
Rehnfors <112483307+Rehnfors@users.noreply.github.com>
2023-02-10 16:34:39 +0100

    Create Metronome.cpp

---
+++ Metronome.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    float ticks, turns;
+    
+    cin >> ticks;
+    
+    turns = ticks/4;
+    
+    cout << turns << endl;
+}
