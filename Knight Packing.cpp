410aeb979210c2ccd8448e5011a8261b4e14691c
Rehnfors <112483307+Rehnfors@users.noreply.github.com>
2023-02-11 10:22:56 +0100

    Create Knight Packing.cpp

---
+++ Knight Packing.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    int size;
+    
+    cin >> size;
+    
+    if(size%2 != 0)
+    {
+        cout << "first" << endl;
+    }
+    else
+    {
+        cout << "second" << endl;
+    }
+}
